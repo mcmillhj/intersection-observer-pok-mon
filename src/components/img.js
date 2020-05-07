9ee19aa580ecf5b142af18cd04ea0263fd429414
Hunter McMillen <hunter.mcmillen@willowtreeapps.com>
2020-05-07 13:54:25 -0400

    intersection observer

--- src/components/img.js
+++ src/components/img.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, createRef } from "react";
 
-const Img = ({ src, alt, ...props }) => {
-  return <img {...props} src={src} alt={alt} className={"img"} />;
+const Img = ({ src, alt, lazy = true, ...props }) => {
+  const imageRef = createRef();
+
+  useEffect(() => {
+    if (imageRef.current && window.observer && lazy) {
+      window.observer.observe(imageRef.current);
+    }
+  });
+
+  return (
+    <img ref={imageRef} {...props} data-src={src} alt={alt} className={"img"} />
+  );
 };
 
 export default Img;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,31 @@ import ReactDOM from "react-dom";
 import "index.css";
 import Grid from "components/grid";
 
+if ("IntersectionObserver" in window) {
+  window.observer = new IntersectionObserver(
+    (entries, self) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log(entry);
+          // load image
+          const img = entry.target;
+          const src = img.getAttribute("data-src");
+          if (!src) {
+            return;
+          }
+          img.src = src;
+
+          // stop observing after image begins to load
+          self.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: [0.2, 1],
+    }
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <Grid />
